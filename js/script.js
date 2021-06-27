500ea8c3ed7a477d7f689008ae4803807e36cb9c
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-06-27 19:03:38 +0300

    linked pizza name event

--- js/script.js
+++ js/script.js
@@ -2,21 +2,27 @@
 $(document).ready(function(){
 
     var pizzas = [
-        {id: "pepperoni", img: "./images/pepperoni.png"}, 
-        {id: "hawaiian", img: "./images/hawaiian.png"}, 
-        {id: "margherita", img: "./images/margherita.png"},
-        {id: "chicken", img: "./images/chicken.png"},
-        {id: "sausage", img: "./images/sausage.png"},
-        {id: "bbq", img: "./images/bbq.png"}
+        {name: "Pepperoni", id: "pepperoni", img: "./images/pepperoni.png"}, 
+        {name: "Hawaiian", id: "hawaiian", img: "./images/hawaiian.png"}, 
+        {name: "Margherita", id: "margherita", img: "./images/margherita.png"},
+        {name: "Chicken Tikka", id: "chicken", img: "./images/chicken.png"},
+        {name: "Sausage & Kale", id: "sausage", img: "./images/sausage.png"},
+        {name: "Bbq Beef", id: "bbq", img: "./images/bbq.png"}
     ];
+
+
 
     // Loops over each pizza and adds image to modal
     pizzas.forEach(function(pizza){
         $('#' + pizza.id + '-btn').click(function(){
             $('#pizza-img').prepend('<img src=' + pizza.img + ' id=' + pizza.id + '>');
+
+            $('#pizza-name').prepend('<h2 id=pepperoni-title>' + pizza.name + '</h2>');
         });
         $("#myModal").on('hidden.bs.modal', function(){
             $('#' + pizza.id).remove();
+
+            $('#' + pizza.id + '-title').remove();
         });
     });
 
@@ -24,8 +30,4 @@ $(document).ready(function(){
 
 });
 
-// $('#myModal').on('hidden.bs.modal', function () {
-//     $('#pizza-img').hide();
-// })
-
-// $('#pizza-img').prepend($('<img>',{id:'pepperoni-img',src:'./images/Pepperoni.png'}));
+// Append to div of id pizza-name
