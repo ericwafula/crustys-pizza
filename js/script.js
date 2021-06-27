cf387474ec12456a860cdea9eec9fcf0f17b5554
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-06-27 19:59:26 +0300

    linked pizza prices

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 $(document).ready(function(){
 
     var pizzas = [
-        {name: "Pepperoni", id: "pepperoni", img: "./images/pepperoni.png"}, 
-        {name: "Hawaiian", id: "hawaiian", img: "./images/hawaiian.png"}, 
-        {name: "Margherita", id: "margherita", img: "./images/margherita.png"},
-        {name: "Chicken Tikka", id: "chicken", img: "./images/chicken.png"},
-        {name: "Sausage & Kale", id: "sausage", img: "./images/sausage.png"},
-        {name: "Bbq Beef", id: "bbq", img: "./images/bbq.png"}
+        {name: "Pepperoni", id: "pepperoni", img: "./images/pepperoni.png", price: 950}, 
+        {name: "Hawaiian", id: "hawaiian", img: "./images/hawaiian.png", price: 550}, 
+        {name: "Margherita", id: "margherita", img: "./images/margherita.png", price: 450},
+        {name: "Chicken Tikka", id: "chicken", img: "./images/chicken.png", price: 850},
+        {name: "Sausage & Kale", id: "sausage", img: "./images/sausage.png", price: 650},
+        {name: "Bbq Beef", id: "bbq", img: "./images/bbq.png", price: 750}
     ];
 
 
@@ -18,11 +18,15 @@ $(document).ready(function(){
             $('#pizza-img').prepend('<img src=' + pizza.img + ' id=' + pizza.id + '>');
 
             $('#pizza-name').prepend('<h2 id=pepperoni-title>' + pizza.name + '</h2>');
+
+            $('#initial-price-pre-text').append('<h2 id="initial-price-text"> Ksh. ' + pizza.price + '</h2>');
         });
         $("#myModal").on('hidden.bs.modal', function(){
             $('#' + pizza.id).remove();
 
             $('#' + pizza.id + '-title').remove();
+
+            $('#initial-price-text').remove();
         });
     });
 
